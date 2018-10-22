ece0e9f4dc283953c61cc26b8d7e27107b1c914d
Canf <mlodzik.ania@gmail.com>
2018-10-22 17:33:08 +0200

    Add CSS to the Posts new.html.erb template

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,21 @@ before_action :redirect_if_not_signed_in, only: [:new]
 		@post = Post.find(params[:id])
 end
 
+def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+   def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post) 
+    else
+      redirect_to root_path
+    end
+  end
+
   def hobby
     posts_for_branch(params[:action])
   end
@@ -22,7 +37,10 @@ end
 
 private
 
-
+def post_params
+  params.require(:post).permit(:content, :title, :category_id)
+                       .merge(user_id: current_user.id)
+end
 
 def posts_for_branch(branch)
   @categories = Category.where(branch: branch)
@@ -31,44 +49,21 @@ def posts_for_branch(branch)
   format.html
   format.js { render partial: 'posts/posts_pagination_page' }
 end
-def post_params
-  params.require(:post).permit(:content, :title, :category_id)
-                       .merge(user_id: current_user.id)
-end
+
 
 end
 
 def get_posts
-  branch = params[:action]
-  search = params[:search]
-  category = params[:category]
-
-  if category.blank? && search.blank?
-    posts = Post.by_branch(branch).all
-  elsif category.blank? && search.present?
-    posts = Post.by_branch(branch).search(search)
-  elsif category.present? && search.blank?
-    posts = Post.by_category(branch, category)
-  elsif category.present? && search.present?
-    posts = Post.by_category(branch, category).search(search)
-  else
-  end
+    PostsForBranchService.new({
+      search: params[:search],
+      category: params[:category],
+      branch: params[:action]
+    }).call
 end
 
-def new
-    @branch = params[:branch]
-    @categories = Category.where(branch: @branch)
-    @post = Post.new
-  end
 
-  def create
-    @post = Post.new(post_params)
-    if @post.save 
-      redirect_to post_path(@post) 
-    else
-      redirect_to root_path
-    end
-  end
+
+ 
 
 
 
